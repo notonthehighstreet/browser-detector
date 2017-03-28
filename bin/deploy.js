a506dc0e62248babd6dcd48a886fa9ccc62bb170
Gil Filipe <gil.filipe@mindera.com>
2017-03-28 17:02:20 +0100

    Added AWS configs for CI and fix some codeclimate issues

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,41 +1,50 @@
 const s3 = require('s3');
 const fs = require('fs-extra');
 
-const client = s3.createClient({
+function buildS3Options() {
+  return {
     s3Options: {
-        accessKeyId: process.env.DETECTOR_AWS_ACCESS_KEY,
-        secretAccessKey: process.env.DETECTOR_AWS_SECRET_KEY
+      accessKeyId: process.env.DETECTOR_AWS_ACCESS_KEY,
+      secretAccessKey: process.env.DETECTOR_AWS_SECRET_KEY
     }
-});
+  };
+}
+
+const options = process.env.DETECTOR_AWS_ACCESS_KEY && process.env.DETECTOR_AWS_SECRET_KEY && buildS3Options();
+const client = s3.createClient(options || {});
 
 const targetDir = './dist/' + process.env.npm_package_version;
 
-fs.ensureDir(targetDir, function (err) {});
+fs.ensureDir(targetDir, function (err) {
+  console.error('unable to ensure dir', err);
+});
 
 try {
-    fs.copySync('./dist/detector.min.js', targetDir+'/detector.min.js');
-} catch (err) {}
+  fs.copySync('./dist/detector.min.js', targetDir + '/detector.min.js');
+} catch (err) {
+  console.error('unable to copy file', err);
+}
 
 const params = {
-    localDir: './dist',
-    s3Params: {
-        ACL:'public-read',
-        Bucket: process.env.DETECTOR_AWS_BUCKET,
-        CacheControl: 'max-age=31536000'
-    }
+  localDir: './dist',
+  s3Params: {
+    ACL: 'public-read',
+    Bucket: process.env.DETECTOR_AWS_BUCKET,
+    CacheControl: 'max-age=31536000'
+  }
 };
 
 const uploader = client.uploadDir(params);
-uploader.on('error', function(err) {
-    console.error('unable to upload:', err.stack); // eslint-disable-line no-console
+uploader.on('error', function (err) {
+  console.error('unable to upload:', err.stack); // eslint-disable-line no-console
 });
-uploader.on('progress', function() {
-    console.log('progress');
+uploader.on('progress', function () {
+  console.log('progress');
 });
-uploader.on('end', function() {
-    console.log('removing temp files...');
-    fs.removeSync(targetDir, function (err) {
-        if (err) return console.error(err)
-    });
-    console.log('done uploading'); // eslint-disable-line no-console
+uploader.on('end', function () {
+  console.log('removing temp files...');
+  fs.removeSync(targetDir, function (err) {
+    if (err) return console.error(err)
+  });
+  console.log('done uploading'); // eslint-disable-line no-console
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: './dist',
-        filename: 'detector.min.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                loaders: ["style-loader", "css-loader" , "sass-loader"]
-            },
-            {
-                test: /\.((woff(2)?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|png|ico|woff)$/,
-                loader: 'url-loader'
-            },
-            {
-                test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/,
-                loader: 'file-loader'
-            },
-        ]
-    }
+  entry: "./src/index.js",
+  output: {
+    path: './dist',
+    filename: 'detector.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.((woff(2)?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|png|ico|woff)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/,
+        loader: 'file-loader'
+      }
+    ]
+  }
 };
